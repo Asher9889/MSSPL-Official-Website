472fe2c8adb6a5f75290d4fd265c53e1dc483f48
Asher9889 <saurabhkushwaha9889@gmail.com>
2024-12-21 18:37:39 +0530

    Top header complete

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,3 @@
-
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import { IoLocationOutline } from "react-icons/io5";
 import { MdEmail } from "react-icons/md";
@@ -9,114 +8,65 @@ import { forwardRef } from "react";
 // const footer = forwardRef(()=> ())
 
 /* eslint-disable */
-const Footer = forwardRef((_,ref) => {
-
-    const handleMarkerClick = () => {
-         googleMapsURL = "https://www.google.com/maps/place/Multifacet+Software+Systems+Pvt+Ltd/@26.4726595,80.3236151,49m/data=!3m1!1e3!4m6!3m5!1s0x399c3880bcee05c7:0xd7ffc1af02582e8c!8m2!3d26.4726406!4d80.3239828!16s%2Fg%2F11fy4v6lrc?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D";
-        window.open(googleMapsURL, "_blank");
-      };
+const Footer = forwardRef((_, ref) => {
+  const handleMarkerClick = () => {
+    googleMapsURL =
+      "https://www.google.com/maps/place/Multifacet+Software+Systems+Pvt+Ltd/@26.4726595,80.3236151,49m/data=!3m1!1e3!4m6!3m5!1s0x399c3880bcee05c7:0xd7ffc1af02582e8c!8m2!3d26.4726406!4d80.3239828!16s%2Fg%2F11fy4v6lrc?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D";
+    window.open(googleMapsURL, "_blank");
+  };
 
   return (
-    <footer ref={ref} className="pt-10 pb-20 bg-zinc-900 pt-32">
+    <footer ref={ref} className=" pb-20 bg-zinc-950 pt-32">
       <ContentWrapper>
-        {/* container div */}
-        {/* <div className=""> */}
-            <div className="flex flex-col md:flex-row gap-10 text-white">
-                <div className="flex flex-col gap-8">
-                <h1 className="poppins-bold text-[40px] text-white">
-                Multifacet Software Systems Pvt. Ltd.
-                </h1>
+      
+        <div className="flex flex-col md:flex-row gap-10 text-white">
+          <div className="flex-1 flex flex-col gap-8">
+            <h1 className="poppins-bold text-[40px] text-white">
+              Multifacet Software Systems Pvt. Ltd.
+            </h1>
 
-                <span className="flex gap-2 flex-row items-center ">
-                    {/* <img src={"msg"} alt="" /> */}
-                    <IoLocationOutline className="text-3xl" />
-                    <p onClick={handleMarkerClick} className="text-[20px] poppins-medium cursor-pointer">
-                    
-                    109/421,80 feet road, Kanpur(U.P),India
-                    </p>
-                </span>
-                <span className="flex gap-2 flex-row items-center">
-                    {/* <img src={"msg"} alt="" />
-                                    < */}
-                    <MdEmail className="text-3xl" />
-                    <p href="mailto:msspl.mspl@gmail.com" className="text-[20px] poppins-medium cursor-pointer">
-                    msspl.mspl@gmail.com
-                    </p>
-                </span>
-
-                <span className="flex gap-2 flex-row items-center">
-                    {/* <img src={"call"} alt="" /> */}
-                    <IoIosCall className="text-3xl" />
-                    <p className="text-[20px] poppins-medium cursor-pointer">
-                    +91-9336810652, 9335148620, 9621509393
-                    </p>
-                </span>
-
-                <span className="w-fit">
-                  <SocialLinks />
-                </span>
-                </div>
-                <div className="flex-1 drop-shadow-xl rounded-xl">
-                  <MapComponent />
-                </div>
-
-            </div>
-
-          
-
-          {/* <div className="flex flex-1 gap-4 md:items-center items-center  flex-col poppins-regular">
-                        <p className="text-2xl">Newsletter</p>
-                        <p className="text-md">stay Up To Date</p>
-                        <div className="flex flex-row mt-6 items-center ">
-                            
-                            <input className="outline-none border-0 bg-zinc-500" type="email" placeholder="Your Email" />
-                            <button className="text-md rounded-md px-4 py-2 text-white bg-black ">Subscribe</button>
-                        </div>
-
-                    </div> */}
-        {/* </div> */}
-
-        {/* <div className="flex flex-1 justify-between  flex-row mt-10">
-            <div className=" flex flex-col items-start gap-4 poppins-regular">
-              <p className="text-2xl hover:animate-pulse font-bold text-[--main-text-color]">
-                Links
+            <span className="flex gap-2 flex-row items-center ">
+             
+              <IoLocationOutline className="text-3xl" />
+              <p
+                onClick={handleMarkerClick}
+                className="text-[20px] poppins-medium cursor-pointer"
+              >
+                109/421,80 feet road, Kanpur(U.P),India
               </p>
-              {options.map((option) => (
-                <button key={option} onClick={()=> handleLinkNavigation(option.toLocaleLowerCase())}  className="text-md hover:cursor-pointer  hover:text-[--main-text-color] hover:underline hover:scale-[1.1] hover:font-medium">
-                  {option}
-                </button>
-              ))}
-            </div>
-
-            <div className="flex flex-col items-start gap-4 poppins-regular">
-              <p className="text-2xl hover:animate-pulse font-bold text-[--main-text-color]">
-                Product
+            </span>
+            <span className="flex gap-2 flex-row items-center">
+              
+              <MdEmail className="text-3xl" />
+              <p
+                href="mailto:msspl.mspl@gmail.com"
+                className="text-[20px] poppins-medium cursor-pointer"
+              >
+                msspl.mspl@gmail.com
               </p>
-              {products.map((product) => (
-                <button key={product} className="text-md hover:cursor-pointer  hover:text-[--main-text-color] hover:underline hover:scale-[1.1]  hover:font-medium">
-                  {product}
-                </button>
-              ))}
-            </div>
+            </span>
 
-            <div className=" flex flex-col items-start gap-4 poppins-regular">
-              <p className="text-2xl hover:animate-pulse font-bold text-[--main-text-color]">
-                Legal
+            <span className="flex gap-2 flex-row items-center">
+              {/* <img src={"call"} alt="" /> */}
+              <IoIosCall className="text-3xl" />
+              <p className="text-[20px] poppins-medium cursor-pointer">
+                +91-9336810652, 9335148620, 9621509393
               </p>
-              <button className="text-md hover:cursor-pointer">
-                Terms of Use
-              </button>
-              <button className="text-md hover:cursor-pointer">
-                Privacy Policy
-              </button>
-              <button className="text-md hover:cursor-pointer">
-                Cookie Policy
-              </button>
-            </div>
-          </div> */}
-        {/* <img src={"footer"} alt="" /> */}
+            </span>
+
+            
+          </div>
+          <div className="flex-1  bg-black">
+            <MapComponent />
+          </div>
+        </div>
+        <div className="w-fit mt-6">
+              <SocialLinks />
+          </div>
       </ContentWrapper>
-      <p className="text-center text-sm poppins-regular mt-20">Copyright 2005 MSSPL All Rights Reserved.</p>
+      <p className="text-center text-sm poppins-regular mt-20">
+        Copyright 2005 MSSPL All Rights Reserved.
+      </p>
     </footer>
   );
 });
--- src/components/header-top-links/HeaderTopLinks.jsx
+++ src/components/header-top-links/HeaderTopLinks.jsx
@@ -5,15 +5,18 @@ import { IoMailSharp } from "react-icons/io5";
 
 const HeaderTopLinks = () =>{
     return (
+        <div className="hidden md:block">
+
         <ContentWrapper>
 
-        <div className=" flex flex-row items-center gap-4 text-[--main-text-color] text-sm py-2 poppins-regular px-6">
-            <span className="flex flex-row items-center gap-2"><MdLocationPin className="blink"/> 109/421,80 feet road, Kanpur(U.P),India </span>
-            <span className="flex flex-row items-center gap-2"><PiPhoneCall />+91-9336810652, 9621509393</span>
-            <span className="flex flex-row items-center gap-2"> <IoMailSharp />msspl.mspl@gmail.com</span>
+        <div className=" flex flex-row items-center justify-center gap-4 text-[--main-text-color] text-sm py-2 poppins-regular px-6">
+            <span className="flex flex-row items-center gap-2"><MdLocationPin className="text-black"/> 109/421,80 feet road, Kanpur(U.P),India </span>
+            <span className="flex flex-row items-center gap-2"><PiPhoneCall  className="text-black"/>+91-9336810652, 9621509393</span>
+            <span className="flex flex-row items-center gap-2"> <IoMailSharp  className="text-black"/>msspl.mspl@gmail.com</span>
          
         </div>
         </ContentWrapper>
+        </div>
     )
 }
 
